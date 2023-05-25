019071fe617abba0c169ea6f3bd74e4d17bba575
정재원 <wjdwodnjs23@naver.com>
2023-05-25 18:16:39 +0900

    back first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import LoginPage from './pages/Login';
+import RootLayout from './pages/Root';
+import MainPage from './pages/Main';
+import PostDetailPage from './pages/PostDetail';
+import PostWritePage from './pages/PostWrite';
 
+const router = createBrowserRouter([
+  {
+    path  : '/',
+    element : <RootLayout></RootLayout>,
+    children : [
+      {
+        index : true,
+        element : <MainPage></MainPage>
+      },
+      {
+        path : "post",
+        children: [
+          {
+            path : ":postId",
+            element: <PostDetailPage/>,
+          },
+          {
+            path : "categoty?name=none",
+            element : <MainPage></MainPage>
+          },
+          {
+            path : "write",
+            element : <PostWritePage></PostWritePage>,
+            //action : 
+          }
+        ]
+      },
+    ]
+    
+  },
+  {
+    path : "/login",
+    element : <LoginPage></LoginPage>
+  }
+])
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
